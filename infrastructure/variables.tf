da6fb3f54bd64129468278b572418a24c365a816
Simon Lynch <srlynch@gmail.com>
2022-10-31 15:12:13 +1100

    update infra

---
+++ infrastructure/variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  type    = string
+  default = "ap-southeast-2"
+}
+
+variable "subnet_pub1" {
+  description = "Public Subnet 1"
+  default     = "subnet-01a543e54f9331473"
+}
+
+variable "sg_attach" {
+  type        = list(any)
+  description = "Security Groups to attach"
+  default     = ["sg-03f7982a72a0ab724", "sg-03c5de6a05e835a26"]
+}
+
+variable "user_data" {
+  type    = string
+  default = <<-EOF
+    #cloud-config
+    runcmd:
+    - cd /tmp
+    - git clone https://github.com/hashicorp-demo-lab/demo-static-content.git;
+    - cd /var/www/html;
+    - sudo cp -r /tmp/demo-static-content/. .;
+    - sudo systemctl restart nginx;
+    EOF
+}
